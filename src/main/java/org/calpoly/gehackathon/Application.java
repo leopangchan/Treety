96b1e2a8a8eea6868eab48029ba8e0b6f7c961c7
leopangchan <leopangchan@gmail.com>
2018-03-27 16:40:32 -0700

    Rename to calpoly-gehackathon

---
+++ src/main/java/org/calpoly/gehackathon/Application.java
@@ -0,0 +1,17 @@
+package org.calpoly.gehackathon;
+
+import org.calpoly.gehackathon.config.SpringApplicationContextInitializer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).
+                initializers(new SpringApplicationContextInitializer())
+                .application()
+                .run(args);
+    }
+}
