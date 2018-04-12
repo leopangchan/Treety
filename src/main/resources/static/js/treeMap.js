8e93382cca59277cfe25d5871b9c7639ab1a7824
smannan <smannan95@hotmail.com>
2018-04-11 20:32:34 -0700

    addded location markers to google map

--- src/main/resources/static/js/treeMap.js
+++ src/main/resources/static/js/treeMap.js
@@ -1,12 +1,10 @@
-app.controller("TreeMapController", function($compile, $scope, $http, NgMap, callAjaxFactory){
+app.controller("TreeMapController", function($scope, $http){
 	
 	var vm = this;
 
 	vm.token = token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImxlZ2FjeS10b2tlbi1rZXkiLCJ0eXAiOiJKV1QifQ.eyJqdGkiOiI1MDhjMjE2YzU3ZmY0MGFhODAxYWI0YzFlNDU5ZGQ3OCIsInN1YiI6InNkLmhhY2thdGhvbiIsInNjb3BlIjpbImllLWN1cnJlbnQuUFNELUlFLUlNQUdFLklFLUlNQUdFLkxJTUlURUQuREVWRUxPUCIsImllLWN1cnJlbnQuUFNELUlFLVRSQUZGSUMuSUUtVFJBRkZJQy5MSU1JVEVELkRFVkVMT1AiLCJpZS1jdXJyZW50LlNELUlFLVZJREVPLklFLVZJREVPLkxJTUlURUQuREVWRUxPUCIsInVhYS5yZXNvdXJjZSIsImllLWN1cnJlbnQuUFNELUlFLVBFREVTVFJJQU4uSUUtUEVERVNUUklBTi5MSU1JVEVELkRFVkVMT1AiLCJpZS1jdXJyZW50LlNELUlFLVBBUktJTkcuSUUtUEFSS0lORy5MSU1JVEVELkRFVkVMT1AiLCJpZS1jdXJyZW50LlNELUlFLUlNQUdFLklFLUlNQUdFLkxJTUlURUQuREVWRUxPUCIsImllLWN1cnJlbnQuUFNELUlFLVBBUktJTkcuSUUtUEFSS0lORy5MSU1JVEVELkRFVkVMT1AiLCJpZS1jdXJyZW50LlBTRC1JRS1WSURFTy5JRS1WSURFTy5MSU1JVEVELkRFVkVMT1AiLCJpZS1jdXJyZW50LlNELUlFLVRSQUZGSUMuSUUtVFJBRkZJQy5MSU1JVEVELkRFVkVMT1AiLCJpZS1jdXJyZW50LlNELUlFLUVOVklST05NRU5UQUwuSUUtRU5WSVJPTk1FTlRBTC5MSU1JVEVELkRFVkVMT1AiLCJpZS1jdXJyZW50LlBTRC1JRS1FTlZJUk9OTUVOVEFMLklFLUVOVklST05NRU5UQUwuTElNSVRFRC5ERVZFTE9QIiwiaWUtY3VycmVudC5TRC1JRS1QRURFU1RSSUFOLklFLVBFREVTVFJJQU4uTElNSVRFRC5ERVZFTE9QIl0sImNsaWVudF9pZCI6InNkLmhhY2thdGhvbiIsImNpZCI6InNkLmhhY2thdGhvbiIsImF6cCI6InNkLmhhY2thdGhvbiIsImdyYW50X3R5cGUiOiJjbGllbnRfY3JlZGVudGlhbHMiLCJyZXZfc2lnIjoiNWE0YzhlYyIsImlhdCI6MTUyMzQwMjAyNiwiZXhwIjoxNTI0MDA2ODI2LCJpc3MiOiJodHRwczovLzYyNGVmZjAyLWRiYjEtNGM2Yy05MGJjLWZhODVhMjllNWZhOC5wcmVkaXgtdWFhLnJ1bi5hd3MtdXN3MDItcHIuaWNlLnByZWRpeC5pby9vYXV0aC90b2tlbiIsInppZCI6IjYyNGVmZjAyLWRiYjEtNGM2Yy05MGJjLWZhODVhMjllNWZhOCIsImF1ZCI6WyJpZS1jdXJyZW50LlNELUlFLUlNQUdFLklFLUlNQUdFLkxJTUlURUQiLCJpZS1jdXJyZW50LlBTRC1JRS1WSURFTy5JRS1WSURFTy5MSU1JVEVEIiwiaWUtY3VycmVudC5QU0QtSUUtVFJBRkZJQy5JRS1UUkFGRklDLkxJTUlURUQiLCJpZS1jdXJyZW50LlBTRC1JRS1QRURFU1RSSUFOLklFLVBFREVTVFJJQU4uTElNSVRFRCIsImllLWN1cnJlbnQuUFNELUlFLVBBUktJTkcuSUUtUEFSS0lORy5MSU1JVEVEIiwiaWUtY3VycmVudC5QU0QtSUUtRU5WSVJPTk1FTlRBTC5JRS1FTlZJUk9OTUVOVEFMLkxJTUlURUQiLCJpZS1jdXJyZW50LlNELUlFLVZJREVPLklFLVZJREVPLkxJTUlURUQiLCJpZS1jdXJyZW50LlNELUlFLVBBUktJTkcuSUUtUEFSS0lORy5MSU1JVEVEIiwiaWUtY3VycmVudC5TRC1JRS1UUkFGRklDLklFLVRSQUZGSUMuTElNSVRFRCIsImllLWN1cnJlbnQuUFNELUlFLUlNQUdFLklFLUlNQUdFLkxJTUlURUQiLCJ1YWEiLCJpZS1jdXJyZW50LlNELUlFLUVOVklST05NRU5UQUwuSUUtRU5WSVJPTk1FTlRBTC5MSU1JVEVEIiwiaWUtY3VycmVudC5TRC1JRS1QRURFU1RSSUFOLklFLVBFREVTVFJJQU4uTElNSVRFRCIsInNkLmhhY2thdGhvbiJdfQ.BumE9iuNyePgtUcJrsVuN-fVVCoO7NVV_fedWN8M4zvoN3EbxIVwq_01Y01IEmQdD6NpWue6N7ER2qvzYBvDISFhJ4XMD9GisQU-xnDRGOT-SxwtUCPiy2UnR0Gqa_jejHBUS11csD-6WUN1ItT3U3i-uasDW4S2KjgRZD5GWXMcx5mvUwqn2R4RF9hereJn_wxd3cNohp8NBlTLSXOw_vAkzl6ha049-U4OjNSYZAfKIWbdtE01oRhic6M_y_4jJkyyJzi9DDFKnuJTFg2IJ09OyGWqlZ6Cqmu95Q7XifjXxWP7_EUhqix2gE-M73bbohwOdDVZ36JjTFoXcUsh9g"
 
-	vm.markers = [];
-	vm.cities = [];
-	vm.chart = ""
+    vm.locationPos = []
 
     /* load a google pie chart */
     vm.loadPieChart = function() {
@@ -49,7 +47,7 @@ app.controller("TreeMapController", function($compile, $scope, $http, NgMap, cal
         var res = []
 
         today = new Date()
-        endts = today.setDate(today.getDate() - 5)
+        endts = today.setDate(today.getDate())
         startts = (new Date(endts)).setDate((new Date(endts)).getDate() - 1)
 
         console.log('start ' + new Date(startts))
@@ -66,11 +64,11 @@ app.controller("TreeMapController", function($compile, $scope, $http, NgMap, cal
             headers: {
                 "Authorization": "Bearer " + vm.token,
                 "Predix-Zone-Id": zoneId,
-                "Access-Control-Allow-Origin": "https://localhost:8090",
+                /*"Access-Control-Allow-Origin": "https://localhost:8090",
                 "Access-Control-Allow-Credentials": "true",
                 "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
                 "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, " +
-                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"*/
             }
         }
 
@@ -131,12 +129,12 @@ app.controller("TreeMapController", function($compile, $scope, $http, NgMap, cal
         var sum = []
         var res = []
 
-        //for (i = days_in_week; i >= 1; i--) {
+        for (i = days_in_week; i >= 1; i--) {
             today = new Date()
             endDate = today.setDate(today.getDate() - 1)
             startDate = (new Date(endDate)).setDate((new Date(endDate)).getDate() - 1)
             vm.getDailyPedestrians(startDate, endDate, res)
-        //}
+        }
 
         return res
     }
@@ -195,57 +193,42 @@ app.controller("TreeMapController", function($compile, $scope, $http, NgMap, cal
         })
     }
 
-	// Ajax Factory request which loads the list of cities.
-	callAjaxFactory.getAjax("getAllCities", '').then(
-		function(response) {
-			vm.cities = response;
-			console.log(vm.cities);
-		}
-	);
-	
-	// Event which shows information about the marked clicked, also performs a zoom in the city
-	vm.showCityInfo = function(event, city){
-		vm.map.setZoom(14);
-		vm.map.setCenter(new google.maps.LatLng(city.latitude, city.longitude));
-		
-		vm.selectedCity = city;
-		vm.map.showInfoWindow('myInfoWindow', this);
-	}
-	
-	// Attribute the NgMap to the map variable from the page
-	// Displaying the markers
-	NgMap.getMap().then(function(map) {
-		vm.map = map;
-		
-		vm.showCustomMarker = function(evt){
-			console.log('showing marker');
-		}
-		
-		vm.closeCustomMarker= function(evt) {
-            this.style.display = 'none';
-        };
-	});
-	
+    // get coordinates for all city sensors
+    vm.getLocations = function() {
+        // query url
+        var metadataurl = 'https://ic-metadata-service-sdhack.run.aws-usw02-pr.ice.predix.io/v2/metadata'
+        var requestURL = metadataurl + '/locations/search?page=0&size=50'
+        var zoneId = 'SD-IE-TRAFFIC'
+        res = []
 
-});
+        var req = {
+            method: 'GET',
+            url: requestURL,
+            headers: {
+                "Authorization": "Bearer " + vm.token,
+                "Predix-Zone-Id": zoneId
+            }
+        }
 
-app.factory('callAjaxFactory', function($http){
-	return{
-		getAjax: function(method, country){
-			
-			var inData = {'country' : country};
-			
-			return $http({
-				url: '/WebAppProject2nd/'+method,
-				method: 'POST',
-				param: inData
-			})
-			.then(
-				function(response){
-					console.log(response.data);
-					return response.data;
-				}
-			);
-		}
-	}
+        $http(req)
+         .then(function(data) {
+            var locations = data.data['content']
+
+            locations.forEach(function(element) {
+                if (element.hasOwnProperty('coordinates')) {
+                    var coord = element['coordinates'].split(":")
+                    res.push([parseFloat(coord[0]), parseFloat(coord[1])])
+                }
+            })
+         })
+         .then(function() {
+            vm.locationPos = res
+            console.log(vm.locationPos)
+         })
+    }
+
+    vm.init = function() {
+        vm.getLocations()
+    }
+
 });
