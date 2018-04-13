810180bd37b68bfd9a68ec2354c50ec3208258db
LarryHu208 <32374419+LarryHu208@users.noreply.github.com>
2018-04-13 00:29:50 -0700

    Commit attempt to add buttons. basic functionality works. format sucks

--- src/main/resources/static/js/treeMap.js
+++ src/main/resources/static/js/treeMap.js
@@ -6,31 +6,120 @@ app.controller("TreeMapController", function($compile, $scope, $http, NgMap, cal
 	vm.markers = [];
 	vm.cities = [];
 
-      vm.loadChart = function() {
+      vm.CrimeloadChart = function() {
       console.log("Load Chart");
         google.charts.load('current', {'packages':['corechart']});
-        google.charts.setOnLoadCallback(this.drawChart);
+        google.charts.setOnLoadCallback(this.drawCrimeChart);
       }
 
-      vm.drawChart = function() {
+      vm.CarbonloadChart = function() {
+            console.log("Load Chart");
+              google.charts.load('current', {'packages':['corechart']});
+              google.charts.setOnLoadCallback(this.drawCarbonChart);
+            }
+
+      vm.TrafficloadChart = function() {
+            console.log("Load Chart");
+              google.charts.load('current', {'packages':['corechart']});
+              google.charts.setOnLoadCallback(this.drawTrafficChart);
+            }
+
+      vm.WaterloadChart = function() {
+                  console.log("Load Chart");
+                    google.charts.load('current', {'packages':['corechart']});
+                    google.charts.setOnLoadCallback(this.drawWaterChart);
+      }
+
+      vm.drawWaterChart = function() {
+
+                      var data = google.visualization.arrayToDataTable([
+                        ['Year', 'Sales', 'Expenses'],
+                        ['2004',  1000,      400],
+                        ['2005',  1170,      460],
+                        ['2006',  660,       1120],
+                        ['2007',  1030,      540]
+                      ]);
+
+                      var options = {
+                        title: 'Company Performance',
+                        curveType: 'function',
+                        legend: { position: 'bottom' }
+                      };
+                      var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+                      chart.draw(data, options);
+
+            }
+
+       vm.drawCarbonChart = function() {
         var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
+                ['Year', 'Visitations', { role: 'style' } ],
+                ['2010', 10, 'color: gray'],
+                ['2020', 14, 'color: #76A7FA'],
+                ['2030', 16, 'opacity: 0.2'],
+                ['2040', 22, 'stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF'],
+                ['2050', 28, 'stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2']
+              ]);
+        var view = new google.visualization.DataView(data);
+              view.setColumns([0, 1,
+                               { calc: "stringify",
+                                 sourceColumn: 1,
+                                 type: "string",
+                                 role: "annotation" },
+                               2]);
+
+        var options = {
+                title: "Carbon Reduced, in metric-tons",
+                width: 600,
+                height: 400,
+                bar: {groupWidth: "95%"},
+                legend: { position: "none" },
+              };
+
+       var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
+        chart.draw(view, options);
+        console.log("I was clicked!")
+      }
+
+      vm.drawCrimeChart = function() {
+        var data = google.visualization.arrayToDataTable([
+          ['Crime', 'Recent (Past Week)'],
+          ['Theft',     11],
+          ['Drugs',      2],
+          ['Hit-Run',  2],
+          ['Violence', 2],
+          ['Ticket',    7]
         ]);
 
         var options = {
-          title: 'My Daily Activities'
+          title: 'Crime'
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
         chart.draw(data, options);
-        console.log("I was clicked!")
+        console.log("Crime: I was clicked!")
       }
+
+       vm.drawTrafficChart = function() {
+              var data = google.visualization.arrayToDataTable([
+                ['Crime', 'Recent (Past Week)'],
+                ['Theft',     11],
+                ['Drugs',      2],
+                ['Hit-Run',  2],
+                ['Violence', 2],
+                ['Ticket',    7]
+              ]);
+
+              var options = {
+                title: 'Traffic'
+              };
+
+              var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+              chart.draw(data, options);
+              console.log("Crime: I was clicked!")
+            }
+
 
 	// Ajax Factory request which loads the list of cities.
 	callAjaxFactory.getAjax("getAllCities", '').then(
