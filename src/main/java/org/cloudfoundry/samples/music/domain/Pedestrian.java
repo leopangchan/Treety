0347777e6028aa8ef042d83bdd337cc7f1497788
leopangchan <leopangchan@gmail.com>
2018-04-16 21:24:23 -0700

    Add pedestrian's repository

---
+++ src/main/java/org/cloudfoundry/samples/music/domain/Pedestrian.java
@@ -0,0 +1,31 @@
+package org.cloudfoundry.samples.music.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Pedestrian {
+
+  public Integer getId() {
+    return id;
+  }
+
+  @Id
+  @Column(length = 40)
+  @GeneratedValue
+  private Integer id;
+
+  public String localId;
+
+  public Long time;
+
+  public Long count;
+
+  public Pedestrian(String localId, Long time, Long count) {
+    this.localId = localId;
+    this.time = time;
+    this.count = count;
+  }
+}
---
+++ src/main/java/org/cloudfoundry/samples/music/repositories/jpa/JpaPedestrianRepository.java
@@ -0,0 +1,14 @@
+package org.cloudfoundry.samples.music.repositories.jpa;
+
+import org.cloudfoundry.samples.music.config.SpringApplicationContextInitializer;
+import org.cloudfoundry.samples.music.domain.Pedestrian;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile({SpringApplicationContextInitializer.LOCAL_PROFILE,
+          SpringApplicationContextInitializer.IN_MEMORY_PROFILE,
+          SpringApplicationContextInitializer.CLOUD_PROFILE})
+public interface JpaPedestrianRepository extends JpaRepository<Pedestrian, Integer> {
+}
